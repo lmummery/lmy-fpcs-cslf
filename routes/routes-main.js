7f59e0cb4fcd97521f4b1f644b3496ec03ea9a73
lukem <lukemummery@outlook.com>
2023-01-03 20:23:27 +0000

    Added basic routes to render pug templates

--- routes/routes-main.js
+++ routes/routes-main.js
@@ -4,47 +4,104 @@ Route handler for CS Lesson Factory web app
 
 module.exports = (app, appData) =>
 {
-	app.get("/", (req, res) => {})
+	app.get("/", (req, res) =>
+	{
+		res.render("index", appData)
+	})
 
-	app.get("/newactivity", (req, res) => {})
+	app.get("/newactivity", (req, res) =>
+	{
+		res.render("newactivity", appData)
+	})
 
-	app.post("/newactivity", (req, res) => {})
+	app.post("/newactivity", (req, res) =>
+	{
+	})
 
-	app.get("/generator", (req, res) => {})
+	app.get("/generator", (req, res) =>
+	{
+		res.render("generator", appData)
+	})
 
-	app.post("/generate", (req, res) => {})
+	app.post("/generate", (req, res) =>
+	{
+	})
 
-	app.get("/lesson/:id", (req, res) => {})
+	app.get("/lesson/:id", (req, res) =>
+	{
+		res.render("lesson", appData)
+	})
 
-	app.get("/mydetails", (req, res) => {})
+	app.get("/mydetails", (req, res) =>
+	{
+		res.render("user-details", appData)
+	})
 
-	app.post("/mydetails", (req, res) => {})
+	app.post("/mydetails", (req, res) =>
+	{
+	})
 
-	app.get("/myactivities", (req, res) => {})
+	app.get("/myactivities", (req, res) =>
+	{
+		res.render("user-activities", appData)
+	})
 
-	app.get("/activity/:id", (req, res) => {})
+	app.get("/activity/:id", (req, res) =>
+	{
+		res.render("activity", appData)
+	})
 
-	app.get("/activity/:id/edit", (req, res) => {})
+	app.get("/activity/:id/edit", (req, res) =>
+	{
+		res.render("activity-edit", appData)
+	})
 
-	app.put("/activity/:id/edit", (req, res) => {})
+	app.put("/activity/:id/edit", (req, res) =>
+	{
+	})
 
-	app.get("/favourites", (req, res) => {})
+	app.get("/favourites", (req, res) =>
+	{
+		res.render("user-favourites", appData)
+	})
 
-	app.get("/lesson/:id/edit", (req, res) => {})
+	app.get("/lesson/:id/edit", (req, res) =>
+	{
+		res.render("lesson-edit", appData)
+	})
 
-	app.put("/lesson/:id/edit", (req, res) => {})
+	app.put("/lesson/:id/edit", (req, res) =>
+	{
+	})
 
-	app.get("/mylessons", (req, res) => {})
+	app.get("/mylessons", (req, res) =>
+	{
+		res.render("user-lessons", appData)
+	})
 
-	app.get("/signup", (req, res) => {})
+	app.get("/signup", (req, res) =>
+	{
+		res.render("signup", appData)
+	})
 
-	app.post("/signup", (req, res) => {})
+	app.post("/signup", (req, res) =>
+	{
+	})
 
-	app.get("/login", (req, res) => {})
+	app.get("/login", (req, res) =>
+	{
+		res.render("login", appData)
+	})
 
-	app.post("/login", (req, res) => {})
+	app.post("/login", (req, res) =>
+	{
+	})
 
-	app.get("/search", (req, res) => {})
+	app.get("/search", (req, res) =>
+	{
+	})
 
-	app.post("/search", (req, res) => {})
+	app.post("/search", (req, res) =>
+	{
+	})
 }
