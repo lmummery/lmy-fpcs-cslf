6d7833d193bb9f3741ff4973112b48716f52668d
Luke Mummery <lukemummery@outlook.com>
2023-02-04 11:49:50 +0000

    Created utility SP to clear activity, lesson and resource tables (for testing only)

---
+++ dev/sql/create_sp.sql
@@ -0,0 +1,15 @@
+/*
+ SQL file to create stored procedure to reset activity, resource and lesson tables
+ */
+
+use fpcs_cslf;
+
+delimiter //
+create procedure wipe_act_les ()
+begin
+	delete from lesson_activity;
+	delete from activity_resource;
+	delete from lesson;
+	delete from resource;
+	delete from activity;
+end //
