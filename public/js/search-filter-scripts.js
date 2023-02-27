8e4dca47ea4cc4b86be7550fafac7dec62b097da
Luke Mummery <lukemummery@outlook.com>
2023-02-27 13:35:29 +0000

    Implemented search filters - year gorup and duration, with clear button

--- public/js/search-filter-scripts.js
+++ public/js/search-filter-scripts.js
@@ -1,38 +1,90 @@
-// radios will always contain the same seven elements
-const radios = [
-	document.getElementById("allyg"), document.getElementById("y1"),
-	document.getElementById("y2"), document.getElementById("y3"),
-	document.getElementById("y4"), document.getElementById("y5"),
-	document.getElementById("y6")
-]
-for (let radio of radios)
+document.getElementById("filter-button").addEventListener("click", activityFilter)
+document.getElementById("min-dur").addEventListener("keydown", e =>
 {
-	radio.addEventListener("click", ygFilter)
+	if (e.key === "Enter") activityFilter()
+})
+document.getElementById("max-dur").addEventListener("keydown", e =>
+{
+	if (e.key === "Enter") activityFilter()
+})
+
+document.getElementById("clear-filters").addEventListener("click", clearFilters)
+
+function activityFilter ()
+{
+	// Start by getting time bounds
+	let minVal = document.getElementById("min-dur").value
+	let maxVal = document.getElementById("max-dur").value
+
+	if (minVal !== "")
+	{
+		console.log(minVal)
+		minVal = parseInt(minVal)
+	} else
+	{
+		console.log("No min value")
+		minVal = 0 // All activities must be longer than 0 minutes
+	}
+
+	if (maxVal !== "")
+	{
+		console.log(maxVal)
+		maxVal = parseInt(maxVal)
+	} else
+	{
+		console.log("No max value")
+		maxVal = 99999 // Large number so no activity could possibly be longer
+	}
+
+	// Get radio elements for year group filter
+	let radios = document.getElementsByName("yg")
+	console.log(radios)
+	let yrVal
+	for (let radio of radios)
+	{
+		if (radio.checked)
+		{
+			console.log(`Selected value = ${radio.value}`)
+			yrVal = radio.value // Only one radio button can ever be selected at a time
+		}
+	}
+
+	// Get all result elements in the list, including any hidden ones
+	const elements = document.getElementById("results-table").children[0].children
+	let el = elements[0]
+	console.log(el.children[0].children[0].children[0].children[1].children[1].innerText.split(" ")[0])
+
+	for (let el of elements)
+	{
+		el.hidden = false
+		// Get just the year group numbers for the current row
+		let elYr = el.children[0].children[0].children[0].children[3].children[0].innerText
+		// Get just the length in minutes for the current activity as a number
+		let elDur = parseInt(el.children[0].children[0].children[0].children[1].children[1].innerText.split(" ")[0])
+
+		if (yrVal !== "0" && !elYr.includes(yrVal))
+		{
+			el.hidden = true
+		}
+		if (elDur < minVal)
+		{
+			el.hidden = true
+		}
+		if (elDur > maxVal)
+		{
+			el.hidden = true
+		}
+	}
+
+	// If all the results are hidden, unhide the message showing that no results match
+	const allHidden = Array.from(elements).filter(el => !el.hidden).length === 0
+	document.getElementById("none-found-msg").hidden = !allHidden;
 }
 
-function ygFilter ()
+function clearFilters ()
 {
-	const elements = document.getElementById("results-table").children[0].children
-	if (this.value === "0")
-	{
-		// Unhide all results when the user selects "All Years"
-		for (let el of elements)
-		{
-			el.hidden = false
-		}
-	}
-	else
-	{
-		let yrstring
-		for (let el of elements)
-		{
-			yrstring = el.children[0].children[0].children[0].children[3].children[0].innerText
-			// If the list of year groups for an activity includes the selected year, it will not be hidden
-			el.hidden = ! yrstring.includes(this.value);
-		}
-	}
-
-	const allHidden = Array.from(elements).filter(el => ! el.hidden).length === 0
-	// If all the results are hidden, unhide the message showing that no results match
-	document.getElementById("none-found-msg").hidden = ! allHidden;
+	document.getElementById("min-dur").value = ""
+	document.getElementById("max-dur").value = ""
+	document.getElementById("allyg").checked = true
+	activityFilter()
 }
